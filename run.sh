6e3686fcf3a5236a78424c2d0ddf527877714347
Mitar <mitar.git@tnode.com>
2015-11-30 03:50:43 -0800

    Updated run script.

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# A script for running this image at the Cloyne deployment. See the README file for details.
+# Moreover, it uses tozd/docker-hosts for service discovery and mounts its volume (/srv/var/hosts).
+
 mkdir -p /srv/var/log/sympa
 mkdir -p /srv/sympa/etc/includes
 mkdir -p /srv/sympa/etc/shared
@@ -7,15 +10,14 @@ mkdir -p /srv/sympa/spool
 mkdir -p /srv/sympa/nullmailer
 mkdir -p /srv/sympa/data
 
-# /srv/sympa/etc/includes/database contains a password for sympa user with
-# permissions over sympa PostgreSQL database at pgsql.server2.docker
-# /srv/sympa/etc/includes/cookie contains crypto cookie string secret
-
-# You can create the database with:
-# createuser -h pgsql.server2.docker -PE -DRS -U postgres -W sympa
-# createdb -h pgsql.server2.docker -U postgres -W -O sympa sympa
-
-# You might have to initialize the database with:
-# psql -h pgsql.server2.docker -U sympa -W -f /usr/share/sympa/bin/create_db.Pg
-
-docker run -d --restart=always --name sympa --hostname sympa -e SET_REAL_IP_FROM=172.17.0.0/16 -e REMOTES=mail -v /srv/var/hosts:/etc/hosts:ro -v /srv/var/log/sympa:/var/log/sympa -v /srv/sympa/etc/includes:/etc/sympa/includes -v /srv/sympa/etc/shared:/etc/sympa/shared -v /srv/sympa/spool:/var/spool/sympa -v /srv/sympa/nullmailer:/var/spool/nullmailer -v /srv/sympa/data:/var/lib/sympa cloyne/sympa
+docker stop sympa || true
+sleep 1
+docker rm sympa || true
+sleep 1
+docker run --detach=true --restart=always --name sympa --hostname sympa \
+  --env SET_REAL_IP_FROM=172.17.0.0/16 --env REMOTES=mail \
+  --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/sympa:/var/log/sympa \
+  --volume /srv/sympa/etc/includes:/etc/sympa/includes --volume /srv/sympa/etc/shared:/etc/sympa/shared \
+  --volume /srv/sympa/spool:/var/spool/sympa --volume /srv/sympa/nullmailer:/var/spool/nullmailer \
+  --volume /srv/sympa/data:/var/lib/sympa \
+  cloyne/sympa
