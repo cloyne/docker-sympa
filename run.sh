d50dd335cd6239ab43b8bda8707ac2e42917f073
Eleanor Cawthon <cawthon.e@gmail.com>
2019-01-08 02:11:23 -0800

    clean up and update run.sh (version agnostic)

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 # A script for running this image at the Cloyne deployment. See the README file for details.
-# Moreover, it uses tozd/docker-hosts for service discovery and mounts its volume (/srv/var/hosts).
 
 mkdir -p /srv/var/log/sympa
 mkdir -p /srv/sympa/etc/includes
@@ -16,9 +15,12 @@ docker rm sympa || true
 sleep 1
 docker run --detach=true --restart=always --name sympa --hostname sympa \
  --env VIRTUAL_HOST=cloyne.org --env VIRTUAL_ALIAS=/lists,/lists-static/ \
- --env SET_REAL_IP_FROM=172.17.0.0/16 --env REMOTES=mail \
- --volume /srv/var/hosts:/etc/hosts:ro --volume /srv/var/log/sympa:/var/log/sympa \
- --volume /srv/sympa/etc/includes:/etc/sympa/includes --volume /srv/sympa/etc/shared:/etc/sympa/shared \
- --volume /srv/sympa/spool:/var/spool/sympa --volume /srv/sympa/nullmailer:/var/spool/nullmailer \
+ --env SET_REAL_IP_FROM=172.18.0.0/16 --env REMOTES=mail \
+ --net="server2.cloyne.org" \
+ --volume /srv/sympa/etc/shared:/etc/sympa/shared \
+ --volume /srv/var/log/sympa:/var/log/sympa \
  --volume /srv/sympa/data:/var/lib/sympa \
+ --volume /srv/sympa/nullmailer:/var/spool/nullmailer \
+ --volume /srv/sympa/spool:/var/spool/sympa \
+ --volume /srv/sympa/etc/includes:/etc/sympa/includes \
  cloyne/sympa
